373236250eb5c4de2b5e8f6ad9c62e18888cb7d7
Galih Hermawan <galih.hermawan@gmail.com>
2021-09-17 00:28:20 +0700

    penyederhanaan lihat data

--- console/lihatData.py
+++ console/lihatData.py
@@ -8,7 +8,6 @@ Created on Thu Sep 16 21:17:37 2021
 from objManajemenBerkas import ManajemenBerkas
 
 nmFile = "dataMahasiswa.txt"
-statusFile = ManajemenBerkas(nmFile).PeriksaBerkas()
 
 mb = ManajemenBerkas(nmFile) 
 data = mb.BacaBerkas() # baca isi file
