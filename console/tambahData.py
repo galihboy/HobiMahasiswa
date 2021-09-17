e6598771028225e0cca305dd2a158de4554a6bb3
Galih Hermawan <galih.hermawan@gmail.com>
2021-09-17 18:43:08 +0700

    penyederhanaan validasi data

--- console/tambahData.py
+++ console/tambahData.py
@@ -22,37 +22,8 @@ for i in data:
 if statusFile:
     file1 = open(nmFile, "a", encoding='utf-8')
     with file1:
-        # validasi NIM
-        while True:
-            nim = input("NIM: ")
-            # periksa format NIM
-            if ut.PeriksaNIM(nim):
-                # periksa apakah NIM sudah terdaftar
-                if ut.PeriksaNIMKembar(nim, kamus):
-                    print("NIM sudah terdaftar.")
-                else:
-                    break
-            else:
-                print("Format NIM tidak sesuai.")
-                
-        # validasi nama
-        while True:
-            nama = input("Nama lengkap: ")
-            # nama tidak boleh kosong
-            if len(nama.strip()) == 0:
-                print("Nama harus diisi")
-            else:
-                break
-        
-        # validasi jenis kelamin
-        while True:
-            jk = input("Jenis kelamin (l/p): ")
-            # isi jenis kelamin harus "l" atau "p"
-            if jk.lower() not in ['l','p']:
-                print("Jenis kelamin harus diisi 'l' atau 'p'")
-            else:
-                break
-        
+        # validasi data baru
+        nim, nama, jk = ut.ValidasiData(kamus)
         isi = [nim, nama, jk.lower()] # masukkan ketiga data dalam list
         isi = "#".join(isi) # gabungkan isi list dengan tanda pemisah "#"
         file1.write(isi) 
