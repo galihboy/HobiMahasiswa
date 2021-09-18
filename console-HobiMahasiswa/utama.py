8ed0eb76d6591b35a097a6edc6e9ad8227b0bf80
Galih Hermawan <galih.hermawan@gmail.com>
2021-09-19 00:44:29 +0700

    tambah menu bersihkan berkas

--- console-HobiMahasiswa/utama.py
+++ console-HobiMahasiswa/utama.py
@@ -5,14 +5,24 @@ Created on Sat Sep 18 21:13:45 2021
 @author: galih-hermawan
 """
 
+from objManajemenBerkas import ManajemenBerkas as mb
+
 while True:
-    print("Aplikasi Pengelolaan Data Hobi Mahasiswa.")
-    pilM = input("[l] lihat, [c] cari, [x] keluar : ") 
+    print("\nAplikasi Pengelolaan Data Hobi Mahasiswa.")
+    pilM = input("\n[l] Lihat Data, \n[c] Cari Data, \
+                  \n[t] Tambah Data, \n[r] Rapikan berkas, \
+                  \n[x] Keluar : ") 
     pilM = pilM.lower().strip()
     if pilM == "l":
         exec(open('lihatData.py').read())
     elif pilM == "c":
         exec(open('cariData.py').read())
+    elif pilM == "t":
+        exec(open('tambahData.py').read())
+    elif pilM == "r":
+        mb("dataHobi.txt").RapikanBerkas()
+        mb("dataMahasiswa.txt").RapikanBerkas()
+        mb("dataMhsHobi.txt").RapikanBerkas()
     elif pilM == "x":
         break
     else:
